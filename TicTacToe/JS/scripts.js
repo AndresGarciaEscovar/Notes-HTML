16802bef1d95789649db704e1c2af0a90e2f9fe0
andres <andrumen1@yahoo.com>
2021-11-14 18:27:47 -0600

    Made progress.

--- TicTacToe/JS/scripts.js
+++ TicTacToe/JS/scripts.js
@@ -1,4 +1,18 @@
 function myFunction(my_id)
 {
- console.log(my_id)
+    // Get the element.
+    var element = document.getElementById(my_id);
+    element.disabled = true;
+
+    // Turn the id into an integer
+    var column = parseInt(my_id) % 3;
+    var row = Math.floor(parseInt(my_id) / 3);
+
+    var coord = [row, column];
+
+    entries[row][column] = "O";
+    console.log(...entries)
+
 }
+
+var entries = [["A", "A", "A"], ["B", "B", "B"], ["C", "C", "C"]];
