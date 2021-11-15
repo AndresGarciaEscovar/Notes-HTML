02652906881b152e8f54d565adc348c9bae087b3
andres <andrumen1@yahoo.com>
2021-11-14 22:24:17 -0600

    Got the icons to change consistently.

--- TicTacToe/JS/scripts.js
+++ TicTacToe/JS/scripts.js
@@ -1,18 +1,45 @@
-function myFunction(my_id)
+
+function changeIcon(cell)
 {
-    // Get the element.
-    var element = document.getElementById(my_id);
-    element.disabled = true;
+        // Block from more moves.
+        cell.disabled = true;
 
-    // Turn the id into an integer
-    var column = parseInt(my_id) % 3;
-    var row = Math.floor(parseInt(my_id) / 3);
+        // Update player 1.
+        if(player1[0])
+        {
+            cell.innerHTML = '<img src='  + player1[1] + '/>';
+            return;
+        }
 
-    var coord = [row, column];
-
-    entries[row][column] = "O";
-    console.log(...entries)
-
+        // Update player 2.
+        if(player1[1] == images[0])
+        {
+            cell.innerHTML = '<img src='  + images[1] + '/>';
+            return;
+        }
+        cell.innerHTML = '<img src='  + images[0] + '/>';
 }
 
-var entries = [["A", "A", "A"], ["B", "B", "B"], ["C", "C", "C"]];
+function makeMove(cellId)
+{
+    // Update the cell.
+    var cell = document.getElementById(cellId);
+    changeIcon(cell);
+
+    // Update the move id.
+    moveId = parseInt(cellId)
+    updateBoard(moveId)
+    player1[0] = !player1[0];
+}
+
+function updateBoard(moveId)
+{
+    // Get the coordinates.
+    var coordinates = [moveId % 3, Math.floor(moveId / 3)];
+    board[coordinates[0]][coordinates[1]] = player1[0]? "P1" : "P2"
+}
+
+
+var board = [[".", ".", "."], [".", ".", "."], [".", ".", "."]];
+var images = ["./Images/icon1.png", "./Images/icon2.png"]
+var player1 = [true, images[0]]
